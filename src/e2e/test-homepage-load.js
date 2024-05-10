e8196dc6becd8c72c47b0d97aafdbaf186b8a99a
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-05-10 14:41:59 +0200

    change Selenium-function to 1 file

--- src/e2e/test-homepage-load.js
+++ src/e2e/test-homepage-load.js
@@ -1,4 +1,4 @@
-const { Builder, until } = require('selenium-webdriver');
+const { Builder, By, until } = require('selenium-webdriver');
 
 async function checkhomepage() {
     let driver = await new Builder().forBrowser('chrome').build();
@@ -11,4 +11,30 @@ async function checkhomepage() {
     }
 }
 
+async function testDynamicContent() {
+    let driver = await new Builder().forBrowser('chrome').build();
+try {
+    await driver.get('http://localhost:4200');
+    let dynamicElement = await driver.wait(until.elementLocated(By.id('dynamic-element-id')), 10000);
+
+    console.log(await dynamicElement.getText());
+
+    await driver.findElement(By.css('button[type="submit"]')).click();
+
+    
+
+} finally {
+    await driver.quit();
+    console.log("driver closed");
+}
+
+}
+
+
+testDynamicContent();
+
+
+/**
+ * To load the Selenium driver: node D:\Arbeiten\Angular\selenium-test-app\src\e2e\test-homepage-load.js
+ **/
 checkhomepage();
